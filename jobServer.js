4bc1252b9db8b1169ed2bf24ec31a284aa413b1c
Ayona Mathew <ayona.mathew@certif-id.com>
2024-12-02 16:27:16 +0100

    new updates

--- jobServer.js
+++ jobServer.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+//const frontendservice = require('./job/src/App.js')
+const path = require('path');
+
 const app = express();
 const port = 5000;
 
@@ -10,15 +13,23 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Have Node serve the files for our built React app
+//This code will first allow Node to access our built React project using the express.static function for static files.
+app.use(express.static(path.resolve(__dirname, 'job/build')));
+
 //basic route set up 
+// Handle GET requests to /api route
+// All other GET requests not handled before will return our React app
+//And if a GET request comes in that is not handled by our /api route, our server will respond with our React app.
 
 app.get('/', (req, res) => {
-    res.send('TalentSure Jobs!');
+    res.sendFile(path.resolve(__dirname, 'job/build', 'index.html'));
 });
 
 // start the server
 
 app.listen(port,() =>{
-    console.log('Congragulations!, The server is running on http://localhost:${port}');
+
+    console.log(`Congragulations!, The server is running on http://localhost:${port}`);
     
 });
